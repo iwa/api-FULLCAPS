8efaa597e86e273cd7f89ea3bd8d890173163def
iwa <iwaongit@gmail.com>
2025-07-31 01:01:46 +0200

    feat: add lowercase route because why not

--- cmd/api-fullcaps/main.go
+++ cmd/api-fullcaps/main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	http.HandleFunc("/", routeRoot)
 	http.HandleFunc("/uppercase", routeUppercase)
+	http.HandleFunc("/lowercase", routeLowercase)
 
 	fmt.Println("Server started at localhost:8080")
 
@@ -40,6 +41,25 @@ func routeUppercase(w http.ResponseWriter, r *http.Request) {
 
 	bodyString := string(bodyBytes)
 	bodyStringUpper := strings.ToUpper(bodyString)
+
+	fmt.Fprint(w, bodyStringUpper)
+}
+
+func routeLowercase(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	bodyString := string(bodyBytes)
+	bodyStringUpper := strings.ToLower(bodyString)
 
 	fmt.Fprint(w, bodyStringUpper)
 }
